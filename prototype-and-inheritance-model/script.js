b7c010888374f9d570e7a3862b5abf4cc04bde4c
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-03-06 20:27:37 -0500

    add a new property to an existing prototype(/class)

--- prototype-and-inheritance-model/script.js
+++ prototype-and-inheritance-model/script.js
@@ -12,9 +12,22 @@ function Person(_name, _age)
     this.age  = _age;
 }
 
-const john = new Person("Jon", 38);
-old_age = john.age;
-john.age++;
-console.log(john.age !== old_age);
+/**
+ * Modify its age
+ */
+const jon = new Person("Jon", 38);
+old_age = jon.age;
+jon.age++;
+console.log(jon.age !== old_age);
+
+/**
+ * Add a new property size to the person prototype.
+ * Set a size to Jon.
+ */
+console.log(jon.size === undefined);
+Person.prototype.size = null;
+console.log(jon.size !== undefined);
+jon.size = 170;
+console.log(jon.size === 170);
 
 console.log("Script finished.");
