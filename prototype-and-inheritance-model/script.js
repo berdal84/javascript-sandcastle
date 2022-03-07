c71a9fab4d57664d2a6d3e5fe11dcdd48fa5b26a
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-03-06 20:43:36 -0500

    object type with methods

--- prototype-and-inheritance-model/script.js
+++ prototype-and-inheritance-model/script.js
@@ -30,4 +30,36 @@ console.log(jon.size !== undefined);
 jon.size = 170;
 console.log(jon.size === 170);
 
+/**
+ * define a PersonWithFunction
+ */
+function PersonWithFunction(_name, _age)
+{
+    console.log(`Create a new Person (name: "${_name}", age: ${_age}yo)`);
+    this.name = _name;
+    this.age  = _age;
+
+    /**
+     * Here we declare 2 functions or "methods" to the prototype.
+     */
+    this.getOlder = function(_years)
+    {
+        console.log(`"${_name}" is getting older ...`);
+        this.age += _years;
+    };
+
+    this.getYounger = function (_years)
+    {
+        console.log(`"${_name}" is getting younger ...`);
+        this.age -= _years;
+    };
+}
+
+const gilles = new PersonWithFunction("Gilles", 38);
+console.log(gilles.age === 38);
+gilles.getYounger(1);
+console.log(gilles.age === 37);
+gilles.getOlder(2);
+console.log(gilles.age === 39);
+
 console.log("Script finished.");
