709d69768101864eb4d300daa4340b281568e76d
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-04-27 09:56:38 -0400

    add doc on testing

--- scripts/testing.js
+++ scripts/testing.js
@@ -1,9 +1,15 @@
 
 let Testing = function (){
-    this.suite;
+
+    this.suite = {};
     this.suiteRes = new Map();
     this.test;
 
+    /**
+     * Begin a new test suite.
+     * endSuite() must be called after all suite tests.  
+     * @param {string} _name suite name
+     */
     this.beginSuite = (_name) => {
         this.suite = {
             name: _name,
@@ -15,26 +21,27 @@ let Testing = function (){
         this.suiteRes.set( _name, this.suite );
     }
 
+    /**
+     * End the current suite and display results.
+     * A suite begins by calling beginSuite("suite name")
+     */
     this.endSuite = () => {
-        let failed  = this.suite.failures.length;
-        let success = this.suite.success.length;
-        let total = failed + success;
+        const failed  = this.suite.failures.length;
+        const success = this.suite.success.length;
+        const total   = failed + success;
 
-        let result;
-        if( failed !== 0) {
-            result = `${failed}/${total} failed!`;
-        } else {
-            result = `${success+failed}/${total} OK`;
-        }
+        const resultStr = failed === 0 ? `${total}/${total} OK` : `${failed}/${total} failed!`;
 
-        console.log(`# Suite "${this.suite.name}": results >>>-------------------------- ${result}`);
-        
-        this.suite.failures.forEach( each => {
-            console.log(`     o "${each.name}" failed !`);
-        })
+        console.log(`# Suite "${this.suite.name}": results >>>-------------------------- ${resultStr}`);        
+        this.suite.failures.forEach( each => { console.log(`     o "${each.name}" failed !`) })
         console.log("\n")
     }
 
+    /**
+     * Begin a new test in current test suite.
+     * A suite must exist, start a new suite with beginSuite("my suite")
+     * @param {string} _name test name 
+     */
     this.beginTest = ( _name ) => {
         this.test = {
             name: _name,
@@ -44,6 +51,10 @@ let Testing = function (){
         console.log(`-- Test: ${this.test.name}`);
     }
 
+    /**
+     * End the current test.
+     * A test must be started, (@see {@link Testing.beginTest})
+     */
     this.endTest = () => {
         
 
@@ -67,23 +78,39 @@ let Testing = function (){
 
 let testing;
 
-let expect = ( testResult, testReason ) => {
-    if( !testResult ) {
+/**
+ * Declare a next expectation.
+ * 
+ * @param {any} expectation an expression evaluated as boolean 
+ * @param {string} reason the reason why expectation must be true
+ */
+let expect = ( expectation, reason ) => {
+    if( Boolean(expectation) ) {
         let failure = {
-            result: testResult,
-            reason: testReason ? testReason : "no reason"
+            result: expectation,
+            reason: reason ? reason : "no reason"
         }
         testing.test.failures.push( failure );
     }
 }
 
-let describe = ( suite_name, tests ) => {
+/**
+ * Describe a test suite
+ * @param {string} name the suite name
+ * @param {function} tests a function containing a set of tests (@see {@link it()} )
+ */
+let describe = ( name, tests ) => {
     testing = new Testing();
-    testing.beginSuite(suite_name);
+    testing.beginSuite(name);
     tests();
     testing.endSuite();
 }
 
+/**
+ * Describe a new test inside a test suite
+ * @param {string} test_name 
+ * @param {function} test a function containing a set of expectations (@see {@link expect()})
+ */
 let it = ( test_name, test ) => {
     testing.beginTest(test_name);
     test();
