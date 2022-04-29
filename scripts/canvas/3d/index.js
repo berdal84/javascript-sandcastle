80625f7df7481e33d1ccc0bfbf72d8f2b8aa6d9b
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-04-29 12:29:56 -0400

    fill canvas with gl.clear()

--- scripts/canvas/3d/index.js
+++ scripts/canvas/3d/index.js
@@ -1,8 +1,31 @@
-const canvas = document.getElementById("canvas");
-console.assert(canvas, "canvas element not found!")
-canvas.width  = 640;
-canvas.height = 480;
 
-const copyright = document.getElementById("copyright");
-console.assert(copyright, "copyright element not found!")
-copyright.innerText = "Canvas using webgl";
+
+function main() {
+    const canvas = document.getElementById("canvas");
+    if( !canvas ) {
+        alert("canvas element not found!");
+        return;
+    }
+    canvas.width  = 640;
+    canvas.height = 480;
+
+    const copyright = document.getElementById("copyright");
+    if( !copyright ) {
+        alert("copyright element not found!");
+        return;
+    }
+    copyright.innerText = "Exercise - HTML5 canvas with webgl context";
+
+    const gl = canvas.getContext('webgl')
+    if( !gl ) {
+        alert("can't get wegl context!");
+        return;
+    }
+
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+window.onload = main;
