ebdbb8381b0e35020009f07838c7d80a1ae7312f
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-04-28 20:59:00 -0400

    bubble sort done !

--- scripts/bubble-sort.js
+++ scripts/bubble-sort.js
@@ -1,23 +1,78 @@
 import { describe, it, expect } from './testing.js'
 
-function bubbleSort( array ) {
+/**
+ * Merge sorted array
+ * @param {Array<number>} _arr0 sorted array (won't be modified)
+ * @param {Array<number>} _arr1 sorted array (won't be modified)
+ * @returns {Array<number>} a sorted merge array
+ */
+function merge(_arr0, _arr1) {
+    // arr0 and arr1 are sorted
 
-    const sort = (arr) => {
-        if( array.length <= 1) {
-            return array;
-        } else if( array.length == 2 ) {
-            if( array[0] > array[1]) {
-                return [ array[1], array[0] ]
-            }
-            return array;
+    console.assert( _arr0 instanceof Array);
+    console.assert( _arr1 instanceof Array);
+    
+    /**
+     * - set two cursors on first item for both arrays.
+     */
+    let cursor0 = 0;
+    let cursor1 = 0;
+    const sorted = [];
+    /*
+    * Until both array items have been sorted (cursor reach the end), we:
+    * - get the smaller pointed number from one or another array.
+    * - increment the pointer from that array
+    * - in case a pointer reach the end, we get the last item from the other
+    */
+    while( cursor0 < _arr0.length || cursor1 < _arr1.length ) {
+        
+        if ( cursor1 == _arr1.length) {
+            sorted.push(_arr0[cursor0]);
+            cursor0++;
+        } else if ( cursor0 == _arr0.length) {
+            sorted.push(_arr1[cursor1]);
+            cursor1++;
+        } else if( _arr0[cursor0] < _arr1[cursor1]) {
+            sorted.push(_arr0[cursor0]);
+            cursor0++;
         } else {
-            // TODO
-            return array;
+            sorted.push(_arr1[cursor1]);
+            cursor1++;
         }
     }
-    console.assert( array instanceof Array);
 
-    return sort(array);
+    console.assert( sorted.length == _arr0.length + _arr1.length )
+
+    return sorted;
+};
+
+/**
+ * Sort an array using bubble sort technique.
+ * @param {Array<number>} _array the array to sort (array won't be modified), can be empty but must be defined. 
+ * @returns {Array<number>} a sorted array
+ */
+function bubbleSort( _array ) {
+
+    console.assert( _array !== undefined);
+    console.assert( _array instanceof Array);
+
+    if( _array.length <= 1) {
+        return _array;
+    } else if( _array.length == 2 ) {
+        if( _array[0] > _array[1]) {
+            return _array.reverse();
+        }
+        return _array;
+    } else {
+        
+        // split array in two
+        const middleIdx = Math.round(_array.length / 2);
+        const arrcpy    = [..._array];    
+        const left      = arrcpy.splice(0, middleIdx );
+        const right     = arrcpy;
+        // sort separately an merge
+        return merge( bubbleSort(left), bubbleSort(right));
+    }
 }
 
 
@@ -36,7 +91,7 @@ describe( "bubble sort",  ()  => {
     })
 
     it( "Should sort many items", ()=> {
-        expect( bubbleSort([9,0,10,15,-2,3,4])).toBe( [-2,0,3,4,9,10,15])
+        expect( bubbleSort([9,0,10,15,-2,3,4])).toBe( [-2,0,3,4,9,10,15], "7 length array should be sorted")
     })
 });
 
