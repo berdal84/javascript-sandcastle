df887223ddec93bf95e2014653daa669f049c981
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-04-29 13:54:58 -0400

    webgl: draw a plane !

--- scripts/canvas/3d/index.js
+++ scripts/canvas/3d/index.js
@@ -1,10 +1,153 @@
 
+const mat4 = glMatrix.mat4;
+
+class Renderer {
+
+    /**
+     * Clear the context
+     * @param {WebGLRenderingContext} gl 
+     */
+    static clear(gl) {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+        gl.clearDepth(1.0);                 // Clear everything
+        gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+        gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    }
+
+    /**
+     * 
+     * @param {WebGLRenderingContext} gl 
+     * @param {*} programInfo 
+     * @param {Plane} plane 
+     */
+    static drawPlane(gl, programInfo, plane) {
+
+        plane.load(gl);
+      
+        // Create a perspective matrix, a special matrix that is
+        // used to simulate the distortion of perspective in a camera.
+        // Our field of view is 45 degrees, with a width/height
+        // ratio that matches the display size of the canvas
+        // and we only want to see objects between 0.1 units
+        // and 100 units away from the camera.
+      
+        const fieldOfView = 45 * Math.PI / 180;   // in radians
+        const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+        const zNear = 0.1;
+        const zFar = 100.0;
+        const projectionMatrix = mat4.create();
+      
+        // note: glmatrix.js always has the first argument
+        // as the destination to receive the result.
+        mat4.perspective(projectionMatrix,
+                         fieldOfView,
+                         aspect,
+                         zNear,
+                         zFar);
+      
+        // Set the drawing position to the "identity" point, which is
+        // the center of the scene.
+        const modelViewMatrix = mat4.create();
+      
+        // Now move the drawing position a bit to where we want to
+        // start drawing the square.
+      
+        mat4.translate(modelViewMatrix,     // destination matrix
+                       modelViewMatrix,     // matrix to translate
+                       [-0.0, 0.0, -6.0]);  // amount to translate
+      
+        // Tell WebGL how to pull out the positions from the position
+        // buffer into the vertexPosition attribute.
+        {
+          const numComponents = 2;  // pull out 2 values per iteration
+          const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+          const normalize = false;  // don't normalize
+          const stride = 0;         // how many bytes to get from one set of values to the next
+                                    // 0 = use type and numComponents above
+          const offset = 0;         // how many bytes inside the buffer to start from
+          gl.bindBuffer(gl.ARRAY_BUFFER, plane.buffers.position);
+          gl.vertexAttribPointer(
+              programInfo.attribLocations.vertexPosition,
+              numComponents,
+              type,
+              normalize,
+              stride,
+              offset);
+          gl.enableVertexAttribArray(
+              programInfo.attribLocations.vertexPosition);
+        }
+      
+        // Tell WebGL to use our program when drawing
+      
+        gl.useProgram(programInfo.program);
+      
+        // Set the shader uniforms
+      
+        gl.uniformMatrix4fv(
+            programInfo.uniformLocations.projectionMatrix,
+            false,
+            projectionMatrix);
+        gl.uniformMatrix4fv(
+            programInfo.uniformLocations.modelViewMatrix,
+            false,
+            modelViewMatrix);
+      
+        {
+          const offset = 0;
+          const vertexCount = plane.vertexCount;
+          gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+        }
+    }
+}
+
+class Plane {
+
+    vertexCount = null;
+    buffers = {
+        position: null
+    }
+
+    constructor() {}
+
+    /**
+     * load plane buffers into gl context
+     * @param {WebGLRenderingContext} gl 
+     * @returns {{position: WebGLBuffer}}
+     */
+    load(gl) {
+
+        const positionBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+      
+        // Now create an array of positions for the square.      
+        const positions = [
+           1.0,  1.0,
+          -1.0,  1.0,
+           1.0, -1.0,
+          -1.0, -1.0,
+        ];
+      
+        this.vertexCount = 4;
+
+        // Now pass the list of positions into WebGL to build the
+        // shape. We do this by creating a Float32Array from the
+        // JavaScript array, then use it to fill the current buffer.      
+        gl.bufferData(gl.ARRAY_BUFFER,
+                      new Float32Array(positions),
+                      gl.STATIC_DRAW);
+      
+        this.buffers.position = positionBuffer;
+
+        return this.buffers;
+    }
+}
 
 class Shader {
 
     vertexShader   = null;
     fragmentShader = null;
-    shaderProgram  = null;
+    program        = null;
 
     constructor( vsSrc, fsSrc ) {
         this.vsSrc = String(vsSrc);
@@ -54,18 +197,18 @@ class Shader {
 
         // program
 
-        this.shaderProgram = gl.createProgram();
-        gl.attachShader(this.shaderProgram, this.vertexShader);
-        gl.attachShader(this.shaderProgram, this.fragmentShader);
-        gl.linkProgram(this.shaderProgram);
+        this.program = gl.createProgram();
+        gl.attachShader(this.program, this.vertexShader);
+        gl.attachShader(this.program, this.fragmentShader);
+        gl.linkProgram(this.program);
         
-        if (!gl.getProgramParameter(this.shaderProgram, gl.LINK_STATUS)) {
-            alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(this.shaderProgram));
-            this.shaderProgram = null;
+        if (!gl.getProgramParameter(this.program, gl.LINK_STATUS)) {
+            alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(this.program));
+            this.program = null;
             return null;
         }
 
-        return this.shaderProgram;
+        return this.program;
     }
 }
 
@@ -91,12 +234,6 @@ function main() {
         return;
     }
 
-    // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    // Clear the color buffer with shaderProgramfied clear color
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-
     const vsSource = `
     attribute vec4 aVertexPosition;
     uniform mat4   uModelViewMatrix;
@@ -116,6 +253,21 @@ function main() {
     if( !shader.load(gl) ) {
         alert("Unable to load Shader!");
     }
+
+    const programInfo = {
+        program: shader.program,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(shader.program, 'aVertexPosition'),
+        },
+        uniformLocations: {
+            projectionMatrix: gl.getUniformLocation(shader.program, 'uProjectionMatrix'),
+            modelViewMatrix: gl.getUniformLocation(shader.program, 'uModelViewMatrix'),
+        },
+    };
+
+    const plane = new Plane();
+    Renderer.clear(gl);
+    Renderer.drawPlane(gl, programInfo, plane);
 }
 
 window.onload = main;
